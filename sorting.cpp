e9f5f4fe6bf6818cf48142d094f76ac3114341f7
shaswot16 <shaswotpaudel58@gmail.com>
2022-11-14 15:27:13 +0545

    quick sort second commit

--- sorting.cpp
+++ sorting.cpp
@@ -1,4 +1,43 @@
-Insertion Sort = Ishan 
-Quick Sort = Sashwot ??
-commit if agree 
-ok 
+#include "sorting.h"
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void generateRandonArray(int arr[], int numOfElement)
+{
+    // Use current time as seed for random generator
+    srand(time(0));
+    for (int i = 0; i < numOfElement; i++)
+    {
+        arr[i] = rand();
+    }
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high + 1;
+    while (i < j)
+    {
+        while (arr[j] < pivot)
+        {
+            j = j - 1;
+        }
+        while (arr[i] > pivot)
+        {
+            i = i + 1;
+        }
+        if (i < j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[low];
+    arr[low] = arr[j];
+    arr[j] = temp;
+}
